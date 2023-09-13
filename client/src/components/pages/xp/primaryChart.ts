ddf75ca7d12b236c3ab42b28af55ee782bdbfaaa
Adam Gastineau <adam@agg.im>
2023-09-12 17:43:45 -0700

    Playing with XP tooltip formatting

--- client/src/components/pages/xp/primaryChart.ts
+++ client/src/components/pages/xp/primaryChart.ts
@@ -1,31 +1,21 @@
 import { useMemo } from "react";
-import * as echarts from "echarts";
-
 import { ALL_SKILLS } from "../../../osrs/types";
+import type {
+  DefaultLabelFormatterCallbackParams,
+  EChartsOption,
+} from "echarts";
+
+import {
+  capitalizeFirstLetter,
+  formatDatetimeNice,
+  formatNumber,
+} from "../../../util/string";
+
+import "./xp.css";
 
 export const usePrimaryChartOptions = () =>
   useMemo(
-    (): echarts.EChartsOption => ({
-      tooltip: {
-        trigger: "axis",
-      },
-      grid: {
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-      },
-      animation: false,
-      series: ALL_SKILLS.map((skill) => ({
-        name: skill,
-        type: "line",
-      })),
-      xAxis: {
-        type: "time",
-      },
-      yAxis: {
-        type: "value",
-      },
+    (): EChartsOption => ({
       dataZoom: [
         {
           type: "inside",
@@ -39,6 +29,48 @@ export const usePrimaryChartOptions = () =>
           end: 10,
         },
       ],
+      animation: false,
+      grid: {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+      },
+      series: ALL_SKILLS.map((skill) => ({
+        id: skill,
+        type: "line",
+      })),
+      tooltip: {
+        trigger: "axis",
+        formatter: (params) => {
+          // We know this is an array because we have multiple series
+          const arrayParams = params as DefaultLabelFormatterCallbackParams[];
+
+          console.log(arrayParams);
+          const timestamp = (arrayParams[0].data as number[])[0];
+
+          const formatedDate = formatDatetimeNice(new Date(timestamp));
+
+          const items = arrayParams
+            .map((param) => {
+              const [_, y] = param.data as number[];
+              return `<div><div>${capitalizeFirstLetter(
+                param.seriesId!
+              )}: ${formatNumber(y)}</div><hr style="background-color: ${
+                param.color
+              }"></div>`;
+            })
+            .join("");
+
+          return `<div class="xpTooltip"><div>${formatedDate}</div><div class="xpTooltipItems">${items}</div></div>`;
+        },
+      },
+      xAxis: {
+        type: "time",
+      },
+      yAxis: {
+        type: "value",
+      },
     }),
     []
   );
--- client/src/util/string.ts
+++ client/src/util/string.ts
@@ -1,2 +1,32 @@
 export const capitalizeFirstLetter = (input: string): string =>
   input.charAt(0).toUpperCase() + input.slice(1);
+
+const numberFormatter = new Intl.NumberFormat();
+
+export const formatNumber = (number: number): string =>
+  numberFormatter.format(number);
+
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "short",
+  timeStyle: "short",
+  hour12: true,
+});
+
+export const formatDatetimeNice = (date: Date): string => {
+  const parts = dateFormatter.formatToParts(date);
+
+  let inHour = false;
+  let dateString = "";
+  let hourString = "";
+  for (const part of parts) {
+    if (part.type === "literal" && part.value === ", ") {
+      inHour = true;
+    } else if (inHour) {
+      hourString += part.value;
+    } else {
+      dateString += part.value;
+    }
+  }
+
+  return `${hourString} on ${dateString}`;
+};
